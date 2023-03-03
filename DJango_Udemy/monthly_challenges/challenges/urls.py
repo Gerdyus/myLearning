7ac2f898309ad1d1530f291602982f17a9e6be8e
Gerdyus <111560219+Gerdyus@users.noreply.github.com>
2023-03-03 13:48:12 +0200

    Reverse, HTML in response

--- DJango_Udemy/monthly_challenges/challenges/urls.py
+++ DJango_Udemy/monthly_challenges/challenges/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("<int:keyword>",views.genericReponseInt),
-    path("<str:keyword>",views.genericResponse)
+    path("",views.index,name = "month-challenge"),
+    path("<int:keyword>", views.genericReponseInt),
+    path("<str:keyword>", views.genericResponse, name = "month-challenge")
 ]
--- DJango_Udemy/monthly_challenges/challenges/views.py
+++ DJango_Udemy/monthly_challenges/challenges/views.py
@@ -1,36 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 month_dictionary = {
-    "january":"Jan",
-    "february":"Feb",
-    "march":"Mar",
-    "april":"Apr",
-    "may":"May",
-    "june":"Jun",
-    "july":"Jul",
-    "august":"Aug",
-    "september":"Sep",
-    "october":"Oct",
-    "november":"Nov",
-    "december":"Dec",
+    "january": "Jan",
+    "february": "Feb",
+    "march": "Mar",
+    "april": "Apr",
+    "may": "May",
+    "june": "Jun",
+    "july": "Jul",
+    "august": "Aug",
+    "september": "Sep",
+    "october": "Oct",
+    "november": "Nov",
+    "december": "Dec",
 }
+
+def index(request):
+    http_response = ""
+    dict_keys = list(month_dictionary.keys())
+    for month in dict_keys:
+        root_url = reverse("month-challenge",args = [month])
+        http_response+= f"<li><a href = \" {root_url} \"> {month.capitalize()} </a></li>"
+          
+    return HttpResponse(http_response)
 
 def genericResponse(request, keyword):
     try:
         returnText = month_dictionary[keyword]
+        return_html = f"<h1>{returnText}</h1>"
     except:
         return HttpResponseNotFound("Unknown parameter keyword.")
-    return HttpResponse(returnText)
+    return HttpResponse(return_html)
 
 
 def genericReponseInt(request, keyword):
-    if(keyword>12):
+    if (keyword > 12):
         return HttpResponseNotFound("Invalid month")
     try:
         dict_keys = list(month_dictionary.keys())
         returnText = dict_keys[keyword-1]
+        # Pretty great - will make /challenge/month (from returnText)
+        redirect_path = reverse("month-challenge", args=[returnText])
     except:
         return HttpResponseNotFound("Unknown parameter keyword.")
-    return HttpResponseRedirect("/challenges/"+returnText)
+    return HttpResponseRedirect(redirect_path)
